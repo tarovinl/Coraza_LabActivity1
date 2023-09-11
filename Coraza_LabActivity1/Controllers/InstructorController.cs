7ce918217078bdb2129a96ec9809bdc766aa91b0
Jhonalyn Cayas <Jhonalyn Cayas@HUAWEI-MATEBOOK>
2023-09-11 23:25:01 +0800

    Add project files.

---
+++ Coraza_LabActivity1/Controllers/InstructorController.cs
@@ -0,0 +1,73 @@
+﻿using Coraza_LabActivity1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coraza_LabActivity1.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+        {
+            new Instructor()
+            {
+                Id = 1,
+                FirstName = "Anton",
+                LastName = "Lee",
+                Rank = Rank.Professor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse ("04/09/2023")
+            },
+            new Instructor()
+            {
+                Id = 2,
+                FirstName = "Kevin",
+                LastName = "Coraza",
+                Rank = Rank.AssistantProfessor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse ("29/09/2022")
+            },
+            new Instructor()
+            {
+                Id = 3,
+                FirstName = "Kirsten",
+                LastName = "Dodgen",
+                Rank = Rank.Instructor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse ("04/01/2023")
+            },
+            new Instructor()
+            {
+                Id = 4,
+                FirstName = "Wonbin",
+                LastName = "Park",
+                Rank = Rank.AssociateProfessor,
+                IsTenured = IsTenured.Probationary,
+                HiringDate = DateOnly.Parse ("29/09/2020")
+            },
+            new Instructor()
+            {
+                Id = 5,
+                FirstName = "Nicholas",
+                LastName = "Galitzine",
+                Rank = Rank.Professor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse ("22/07/2023")
+            }
+
+
+        };
+        public IActionResult Index()
+        {
+            return View(InstructorList);
+        }
+        public IActionResult ShowDetail(int id)
+        {
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+            if (instructor != null)
+            {
+                return View(instructor);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Coraza_LabActivity1/Models/Instructor.cs
@@ -0,0 +1,21 @@
+﻿namespace Coraza_LabActivity1.Models
+{
+
+    public enum Rank
+    {
+        Instructor, AssistantProfessor, AssociateProfessor, Professor
+    }
+    public enum IsTenured
+    {
+        Permanent, Probationary
+    }
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public IsTenured IsTenured { get; set; }
+        public Rank Rank { get; set; }
+        public DateOnly HiringDate { get; set; }
+    }
+}
